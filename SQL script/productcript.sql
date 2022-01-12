438007af0d9e7d46fa837f4c6fe9962d531b627f
orimilbauer <orimilbauer@gmail.com>
2022-01-12 19:35:05 +0200

    change 2 ori- menu
    still need to do:
    1. force only one chosen checkbox
    2. quntity functions and boxes
    3. generate data for the product table (cakes...)
    4. connection with cart

---
+++ SQL script/productcript.sql
@@ -0,0 +1,10 @@
+insert into Products(name,num_of_pcs,price,image_src,is_birthday_cake,is_gluten_free,is_top_seller,is_vegan)
+ values ('Raspberries Cheese Cake','1','15','/static/img/products/cheesecake1.jpg','1','0','1','0');
+insert into Products(name,num_of_pcs,price,image_src,is_birthday_cake,is_gluten_free,is_top_seller,is_vegan)
+ values ('Blue Berries Cheese Cake','2','15','/static/img/products/cheesecake4.jpg','0','0','1','0');
+insert into Products(name,num_of_pcs,price,image_src,is_birthday_cake,is_gluten_free,is_top_seller,is_vegan)
+ values ('Passion Fruit Cheese Cake','3','15','/static/img/products/cheesecake3.jpg','0','0','0','0');
+insert into Products(name,num_of_pcs,price,image_src,is_birthday_cake,is_gluten_free,is_top_seller,is_vegan)
+ values ('Raspberries Cheese Cake','4','15','/static/img/products/cheesecake2.jpg','1','1','1','1');
+
+# truncate  table products;
--- pages/menu/menu.py
+++ pages/menu/menu.py
@@ -1,4 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request,redirect
+from interact_with_DB import interact_db
+import datetime
+from flask import session
+from flask import flash
+import flask
+import math
 
 # menu blueprint definition
 menu = Blueprint('menu', __name__, static_folder='menu', static_url_path='/static', template_folder='templates')
@@ -7,4 +13,45 @@ title = 'Matamim | Menu'
 # Routes
 @menu.route('/menu')
 def index():
-    return render_template('menu.html',title = title)
+    query = "select * from Products ;"
+    products = interact_db(query=query, query_type='fetch')
+    num_of_row=int(math.ceil(len(products)/3))
+    return render_template('menu.html',title = title,temp_products=products,products=products,num_of_row=num_of_row)
+
+@menu.route('/menu_search', methods=['get','post'])
+def menu_search_func():
+    query = "select * from Products ;"
+    # name = request.form['name']
+    if request.form.get("vegan") != None:
+             query = "select * from Products where is_vegan=1 ;"
+    if request.form.get("gluten") != None:
+             query = "select * from Products where is_gluten_free=1;"
+    if request.form.get("birthday") != None:
+             query = "select * from Products where is_birthday_cake=1 ;"
+    if request.form.get("top") != None:
+            query = "select * from Products where is_top_seller=1 ;"
+    if request.form.get("all") != None:
+            query = "select * from Products;"
+    # if request.form.get("vegan") != None:
+    #     is_vegan = '1'
+    # else:
+    #     is_vegan = '0'
+    # if request.form.get("gluten") != None:
+    #     is_gluten_free = '1'
+    # else:
+    #     is_gluten_free = '0'
+    # if request.form.get("birthday") != None:
+    #     is_birthday = '1'
+    # else:
+    #     is_birthday = '0'
+    # if request.form.get("top") != None:
+    #     is_top = '1'
+    # else:
+    #     is_top = '0'
+    # if request.form.get("all") != None:
+    #     is_all = '1'
+    # else:
+    #     is_all = '0'
+    products = interact_db(query=query, query_type='fetch')
+    num_of_row = int(math.ceil(len(products)/ 3))
+    return render_template('menu.html',title = title,products=products,temp_products=products,num_of_row=num_of_row)
